f8643e3836eb962afd4b888a6bbb78762bc59daa
John Nicholas <john.nicholas@dinoroar.io>
2023-03-14 15:49:39 +0000

    killed all them mutants

--- csharpcore-Verify.xunit/GildedRoseTests/ApprovalTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/ApprovalTest.cs
@@ -41,7 +41,21 @@ namespace GildedRoseTests
             var fakeoutput = new StringBuilder();
             var items = new List<Item>()
             {
-                new Item(){Name = "foo", Quality = 0, SellIn = 0}
+                new(){Name = "foo0", Quality = 0, SellIn = 0},
+                new(){Name = "foo1", Quality = 1, SellIn = 0},
+                new(){Name = "foo2", Quality = 2, SellIn = 0},
+                new(){Name = "foo50", Quality = 50, SellIn = 0},
+                new(){Name = "Aged Brie", Quality = 0, SellIn = 1},
+                new(){Name = "Aged Brie", Quality = 48, SellIn = 3},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 0, SellIn = 0},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 0},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 1},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 50, SellIn = 5},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 48, SellIn = 11},
+                new(){Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 47, SellIn = 6},
+                new(){Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 0},
+                new(){Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = -1},
+                new(){Name = "Sulfuras, Hand of Ragnaros", Quality = 50, SellIn = 0},
             };
             Worker.DoStuff(items, 3, (message) => fakeoutput.AppendLine(message));
             var output = fakeoutput.ToString();
