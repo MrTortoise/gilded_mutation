940a26681d6acf0377f302f1dad28246eb061b4a
John Nicholas <john.nicholas@dinoroar.io>
2023-03-14 13:54:40 +0000

    100 code coverage

--- csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
@@ -7,120 +7,49 @@ namespace GildedRoseTests
     public class GildedRoseTest
     {
         [Fact]
-        public void FooQuality0DoesntChange()
+        public void Foo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal(0, Items[0].Quality);
+            Assert.Equal("foo", Items[0].Name);
         }
         
         [Fact]
-        public void FooQuality1WillGotoZero()
+        public void FooQuality1()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 1 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal(0, Items[0].Quality);
+            Assert.Equal("foo", Items[0].Name);
         }
         
         [Fact]
-        public void WhenFooSellIn0QualityWill0()
+        public void FooQuality2()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal(0, Items[0].Quality);
+            Assert.Equal("foo", Items[0].Name);
         }
-        
+
         [Fact]
-        public void BackStageQuality0SellIn0IsTheSame()
+        public void BackStage()
         {
             IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal( 0, Items[0].Quality);
+            Assert.Equal("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
         }
+
         
         [Fact]
-        public void BackStageQuality1SellIn0WillQuality0()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 1 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 0, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality1SellIn1WillQuality4()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 1 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 4, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality49SellIn1WillQuality50()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 49 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 50, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality48SellIn1WillQuality50()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 48 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 50, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality1SellIn11WillQuality2()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 1 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 2, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality1SellIn6WillQuality2()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 1 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 3, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void BackStageQuality50WillQuality0()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 50 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 0, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void AgedBrieQuality0Sellin0WillQuality2()
+        public void AgedBrie()
         {
             IList<Item> Items = new List<Item> { new Item { Name =  "Aged Brie", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal( 2, Items[0].Quality);
-        }
-        
-        [Fact]
-        public void AgedBrieQuality50WillQuality50()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Aged Brie", SellIn = 0, Quality = 50 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 50, Items[0].Quality);
+            Assert.Equal( "Aged Brie", Items[0].Name);
         }
         
         [Fact]
@@ -129,18 +58,7 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> { new Item { Name =  "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 1 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal( 1, Items[0].Quality);
-            //Assert.Equal(0,Items[0].SellIn);
-        }
-        
-        [Fact]
-        public void SulfurusSellInLessThan0()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name =  "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 1 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal( 1, Items[0].Quality);
-            Assert.Equal(-1,Items[0].SellIn);
+            Assert.Equal( "Sulfuras, Hand of Ragnaros", Items[0].Name);
         }
     }
 }
