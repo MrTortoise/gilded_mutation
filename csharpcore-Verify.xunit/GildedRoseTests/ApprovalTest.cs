56f3cff4e7e3d5474dee7526d8da79147e7057a6
John Nicholas <john.nicholas@dinoroar.io>
2023-03-14 14:33:50 +0000

    refactor before testing

--- csharpcore-Verify.xunit/GildedRoseTests/ApprovalTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/ApprovalTest.cs
@@ -12,20 +12,20 @@ using Xunit;
 
 namespace GildedRoseTests
 {
-    // [UsesVerify]
-    // public class ApprovalTest
-    // {
-    //     [Fact]
-    //     public Task ThirtyDays()
-    //     {
-    //         var fakeoutput = new StringBuilder();
-    //         Console.SetOut(new StringWriter(fakeoutput));
-    //         Console.SetIn(new StringReader("a\n"));
-    //
-    //         Program.Main(new string[] { });
-    //         var output = fakeoutput.ToString();
-    //
-    //         return Verifier.Verify(output);
-    //     }
-    // }
+    [UsesVerify]
+    public class ApprovalTest
+    {
+        [Fact]
+        public Task ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+    
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+    
+            return Verifier.Verify(output);
+        }
+    }
 }
